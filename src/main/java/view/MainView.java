2ef5aa1276a5c4b5a0cd6353760c314da6af7859
mizukiroo <163668270+mizukiroo@users.noreply.github.com>
2025-06-28 15:27:38 +0300

    меинвью

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -17,7 +17,7 @@ public class MainView {
     public void showStartView() {
         StartController startController = new StartController(stage, this);
         stage.setScene(startController.getScene());
-        stage.setTitle("Добро пожаловать");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
@@ -27,7 +27,7 @@ public class MainView {
     public void showLoginView() {
         LoginController loginController = new LoginController(stage, authController, this);
         stage.setScene(loginController.getScene());
-        stage.setTitle("Авторизация");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
@@ -36,16 +36,13 @@ public class MainView {
 
     public void showRegisterView(String role) {
         RegisterController registerController = new RegisterController(stage, authController, role, this);
-        stage.setTitle("Регистрация - " + role);
+        stage.setTitle("Аренда торговых точек");
         stage.setScene(registerController.getScene());
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
         stage.show();
     }
-
-
-
 
     public void showSuccessView(String username, String role) {
         if (role.equals("admin")) {
@@ -56,22 +53,20 @@ public class MainView {
         }
     }
 
-    // ✅ Окно выбора: Добавить точку / Просмотр договоров / Назад
     public void showAdminPanelStartView(String username) {
         AdminPanelView view = new AdminPanelView(stage, username, this);
         stage.setScene(view.getScene());
-        stage.setTitle("Администратор");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
         stage.show();
     }
 
-    // ✅ Окно добавления торговой точки
     public void showAddPointView(String username) {
         AddTradePointView view = new AddTradePointView(stage, username, this);
         stage.setScene(view.getScene());
-        stage.setTitle("Добавить торговую точку");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
@@ -81,38 +76,50 @@ public class MainView {
     public void showAllContractsView(String username) {
         AllContractsView view = new AllContractsView(stage, username, this);
         stage.setScene(view.getScene());
-        stage.setTitle("Все договоры");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
         stage.show();
     }
+
+    public void showAllPaymentsView(String username) {
+        AllPaymentsView view = new AllPaymentsView(stage, username, this);
+        stage.setScene(view.getScene());
+        stage.setTitle("Аренда торговых точек");
+        stage.setResizable(false);
+        stage.setWidth(550);
+        stage.setHeight(600);
+        stage.show();
+    }
+
 
     public void showProfileView(String username) {
         ClientProfileView profileView = new ClientProfileView(stage, username);
         stage.setScene(profileView.getScene());
-        stage.setTitle("Личный кабинет");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
         stage.show();
     }
 
-//    public void showPaymentView(String username) {
-//        PaymentView paymentView = new PaymentView(stage, username, this);
-//        stage.setTitle("Регистрация - ");
-//        stage.setScene(paymentView.getScene());
-//        stage.setResizable(false);
-//        stage.setWidth(550);
-//        stage.setHeight(600);
-//        stage.show();
-//    }
+    public void showPaymentView(String username) {
+        PaymentView paymentView = new PaymentView(stage, username, this);
+        stage.setScene(paymentView.getScene());
+        stage.setTitle("Аренда торговых точек");
+        stage.setResizable(false);
+        stage.setWidth(550);
+        stage.setHeight(600);
+        stage.show();
+    }
+
 
 
     public void showLeaseView(String username) {
         LeaseView leaseView = new LeaseView(stage, username, this);
         stage.setScene(leaseView.getScene());
-        stage.setTitle("Оформление аренды");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
@@ -122,7 +129,7 @@ public class MainView {
     public void showMyLeasesView(String username) {
         MyLeasesView view = new MyLeasesView(stage, username, this);
         stage.setScene(view.getScene());
-        stage.setTitle("Мои аренды");
+        stage.setTitle("Аренда торговых точек");
         stage.setResizable(false);
         stage.setWidth(550);
         stage.setHeight(600);
